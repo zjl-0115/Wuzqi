1628419599c78c3543272270aba7f6d502421c82
zjl-0115 <2983406448@qq.com>
2025-05-21 15:57:51 +0800

    Update mainwindow.h

--- wuziqi/mainwindow.h
+++ wuziqi/mainwindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include "qipan.h"
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
@@ -19,5 +20,7 @@ public:
 
 private:
     Ui::MainWindow *ui;
+    // 棋盘类
+    qipan * m_qipan;
 };
 #endif // MAINWINDOW_H
