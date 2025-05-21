b96a1b79f736ac13cdd6517b2e4f3f89a21f7ad7
zjl-0115 <2983406448@qq.com>
2025-05-21 15:57:28 +0800

    Update mainwindow.cpp

--- wuziqi/mainwindow.cpp
+++ wuziqi/mainwindow.cpp
@@ -1,11 +1,26 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
+// 垂直布局
+#include<QVBoxLayout>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+
+    // 设置窗口标题
+    setWindowTitle("五子棋");
+    // 设置窗口固定大小
+    setFixedSize(800,600);
+    // 创建棋盘对象
+    m_qipan=new qipan(this);
+    // 设置布局
+    QWidget* centerWidget=new QWidget(this);
+    setCentralWidget(centerWidget);
+    QVBoxLayout* layout = new QVBoxLayout(centerWidget);
+    layout->addWidget(m_qipan);
 }
 
 MainWindow::~MainWindow()
