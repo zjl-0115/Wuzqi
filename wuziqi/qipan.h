95f3a32f52cf13bc80e6367e52de28fc8db366a7
zjl-0115 <2983406448@qq.com>
2025-05-22 17:12:40 +0800

    Update qipan.h

--- wuziqi/qipan.h
+++ wuziqi/qipan.h
@@ -2,17 +2,27 @@
 #define QIPAN_H
 
 #include <QWidget>
+#include <QMouseEvent>
+#include "stdShared.h"
 
-class qipan : public QWidget
+class QiPan : public QWidget
 {
     Q_OBJECT
 public:
-    explicit qipan(QWidget *parent = nullptr);
+    explicit QiPan(QWidget *parent = nullptr);
 protected:
     // 绘画事件
     void paintEvent(QPaintEvent* event) override;
+    // 鼠标移动事件
+    void mouseMoveEvent(QMouseEvent* event) override;
+    // 鼠标点击事件
+    void mousePressEvent(QMouseEvent *event) override;
 signals:
-    public slots:
+public slots:
+
+private:
+    // 判断用户是否获胜
+    bool isCheckWin(int row,int col,ROLE role);
 private:
     // 棋盘尺寸
     static const int m_boardSize=15;
@@ -20,6 +30,15 @@ private:
     static const int m_cellSize=40;
     // 棋盘距离边缘的距离
     static const int m_margin=20;
+
+    // 棋盘上的点
+    ROLE m_board[m_boardSize][m_boardSize];
+
+    // 悬停点坐标
+    int m_hoverRow;
+    int m_hoverCol;
+    // 是否需要绘制悬停点
+    bool m_hoverFlag;
 };
 
 #endif // QIPAN_H
