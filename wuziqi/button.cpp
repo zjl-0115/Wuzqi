49a1dd65aefc2ba5e9a70b4eeee3180e2dc39912
zjl-0115 <2983406448@qq.com>
2025-05-27 23:57:39 +0800

    Update button.cpp

--- wuziqi/button.cpp
+++ wuziqi/button.cpp
@@ -15,7 +15,16 @@ Button::Button(QiPan* qipan, QWidget *parent)
     btn2->move(650,150);
     btn2->setFixedSize(80,40);
 
+    btn3=new QPushButton("结束游戏",qipan);
+    btn3->move(650,200);
+    btn3->setFixedSize(80,40);
+
     // 连接按钮点击信号到自定义信号
-    connect(btn1, &QPushButton::clicked, this, &Button::doublePlayerClicked);
+    connect(btn2, &QPushButton::clicked, this, &Button::doublePlayerClicked);
+    connect(btn2, &QPushButton::clicked, this, &Button::computerPlayerClicked);
+    connect(btn3, &QPushButton::clicked, parent, &QWidget::close);
+
     }
+
+
 }
