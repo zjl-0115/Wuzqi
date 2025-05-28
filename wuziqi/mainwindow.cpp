36d40a150aa4dd07fa59bf46a0187eccfc7e59cf
zjl-0115 <2983406448@qq.com>
2025-05-29 00:03:37 +0800

    Update mainwindow.cpp

--- wuziqi/mainwindow.cpp
+++ wuziqi/mainwindow.cpp
@@ -13,14 +13,25 @@ MainWindow::MainWindow(QWidget *parent)
     // 设置窗口标题
     setWindowTitle("五子棋");
     // 设置窗口固定大小
-    setFixedSize(800,600);
+    setFixedSize(800,650);
     // 创建棋盘对象
-    m_qipan=new qipan(this);
+    m_qipan=new QiPan(this);
+    // 创建按钮面板，传入棋盘指针
+    m_button = new Button(m_qipan, this);
+    // // 创建按钮对象
+    // m_button= new Button(m_qipan,this);
     // 设置布局
     QWidget* centerWidget=new QWidget(this);
     setCentralWidget(centerWidget);
     QVBoxLayout* layout = new QVBoxLayout(centerWidget);
     layout->addWidget(m_qipan);
+
+
+    //连接信号和槽
+    connect(m_button,&Button::doublePlayerClicked,
+            m_qipan,&QiPan::startDoublePlayerGame);
+    connect(m_button, &Button::computerPlayerClicked,
+            m_qipan, &QiPan::startComputerGame);
 }
 
 MainWindow::~MainWindow()
