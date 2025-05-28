72f14362b3cf8563c2f0f9f5add5fb106e9aad61
zjl-0115 <2983406448@qq.com>
2025-05-29 00:04:35 +0800

    Update qipan.h

--- wuziqi/qipan.h
+++ wuziqi/qipan.h
@@ -17,12 +17,13 @@ protected:
     void mouseMoveEvent(QMouseEvent* event) override;
     // 鼠标点击事件
     void mousePressEvent(QMouseEvent *event) override;
-signals:
-public slots:
 
 private:
     // 判断用户是否获胜
     bool isCheckWin(int row,int col,ROLE role);
+    //切换玩家
+    void switchPlayer();
+
 private:
     // 棋盘尺寸
     static const int m_boardSize=15;
@@ -34,11 +35,22 @@ private:
     // 棋盘上的点
     ROLE m_board[m_boardSize][m_boardSize];
 
+    //游戏模式
+    MODE m_gameMode;
+    //当前玩家
+    ROLE m_currentPlayer;
+
     // 悬停点坐标
     int m_hoverRow;
     int m_hoverCol;
     // 是否需要绘制悬停点
     bool m_hoverFlag;
+
+signals:
+
+public slots:
+    void startDoublePlayerGame();  // 开始双人游戏
+    void startComputerGame();      // 开始人机游戏
 };
 
 #endif // QIPAN_H
