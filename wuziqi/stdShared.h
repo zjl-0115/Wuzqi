1b2221aa77c873d2eed5353e0d4ff5d4d654a3d1
zjl-0115 <2983406448@qq.com>
2025-06-04 16:12:08 +0800

    Update stdShared.h

--- wuziqi/stdShared.h
+++ wuziqi/stdShared.h
@@ -3,15 +3,16 @@
 
 enum ROLE
 {
-    EMPTY = 0,      /*没有人下*/
-    PLAYER = 1,     /*玩家落子*/
-    PLAYER2 = 2,    /*玩家2落子*/
-    COMPUTER = 3    /*人机落子*/
+    EMPTY=0,      /*没有人下*/
+    PLAYER=1,     /*玩家落子*/
+    PLAYER2=2,    /*玩家2落子*/
+    COMPUTER=2,   /*人机落子*/
 };
 
 enum MODE {
     SINGLE_PLAYER,
-    DOUBLE_PLAYER
+    DOUBLE_PLAYER,
+    NONE
 };
 
 #endif // STDSHARED_H
